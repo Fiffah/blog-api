8992a9440f033066006caf69f944f178c782ae4a
Fiffah <itsmefiffah@gmail.com>
2024-11-07 10:36:28 +0700

    Initial commit with project files and README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Nama Proyek
+
+API for blog SPA with laravel
+
+## Prerequisites
+
+- PHP >= 8.0
+- Composer
+- Laravel 11
+- Database (misalnya MySQL atau SQLite)
+
+## Installation
+
+1. Clone repositori ini:
+
+   
+   git clone https://github.com/username/repository-name.git
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::post('/posts', [PostController::class, 'store']);
+});
